7dd7e4a82fa11dc90b9d4485e783c750d4b3406b
Dmitry <97835547+greenlarity@users.noreply.github.com>
2024-05-26 15:01:02 +0300

    typed

--- tests/TheInternetTests.spec.ts
+++ tests/TheInternetTests.spec.ts
@@ -1,8 +1,9 @@
-import { expect, test } from '@playwright/test';
-const TARGET_URL = 'http://the-internet.herokuapp.com/add_remove_elements/';
-let countButtons = 15;
+import { Page, expect, test } from '@playwright/test';
+const TARGET_URL: string =
+	'http://the-internet.herokuapp.com/add_remove_elements/';
+let countButtons: number = 15;
 
-test('Click on dynamically added buttons', async ({ page }) => {
+test('Click on dynamically added buttons', async ({ page }: { page: Page }) => {
 	await page.goto(`${TARGET_URL}`);
 	await expect(page).toHaveTitle('The Internet');
 	for (let i = 0; i < countButtons; i++) {
@@ -10,13 +11,12 @@ test('Click on dynamically added buttons', async ({ page }) => {
 	}
 	await page.waitForSelector('.added-manually');
 	const deleteButtons = page.locator('.added-manually');
-	await deleteButtons.count().then(async (count) => {
-		for (let i = 0; i < count; i++) {
-			try {
-				await deleteButtons.nth(0).click();
-			} catch (e) {
-				console.log(`Error clicking on element ${i}: ${e.message}`);
-			}
+	const count: number = await deleteButtons.count();
+	for (let i: number = 0; i < count; i++) {
+		try {
+			await deleteButtons.nth(0).click();
+		} catch (e) {
+			console.log(`Error clicking on element ${i}: ${e.message}`);
 		}
-	});
+	}
 });
