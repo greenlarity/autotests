c254abf084f408c51ed0c63028dc7e8a15c68d7d
Dmitry <97835547+greenlarity@users.noreply.github.com>
2024-05-26 15:05:54 +0300

    update

--- tests/TheInternetTests.spec.ts
+++ tests/TheInternetTests.spec.ts
@@ -1,12 +1,18 @@
 import { Page, expect, test } from '@playwright/test';
-const TARGET_URL: string =
-	'http://the-internet.herokuapp.com/add_remove_elements/';
-let countButtons: number = 15;
+
+type Props = {
+	targetURL: string;
+	countButtons: number;
+};
+let testSettings: Props = {
+	targetURL: 'http://the-internet.herokuapp.com/add_remove_elements/',
+	countButtons: 15,
+};
 
 test('Click on dynamically added buttons', async ({ page }: { page: Page }) => {
-	await page.goto(`${TARGET_URL}`);
+	await page.goto(`${testSettings.targetURL}`);
 	await expect(page).toHaveTitle('The Internet');
-	for (let i = 0; i < countButtons; i++) {
+	for (let i = 0; i < testSettings.countButtons; i++) {
 		await page.getByRole('button', { name: 'Add Element' }).click();
 	}
 	await page.waitForSelector('.added-manually');
